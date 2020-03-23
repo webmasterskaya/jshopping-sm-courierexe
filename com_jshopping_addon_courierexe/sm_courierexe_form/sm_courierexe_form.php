aad2a77f990bd009d19e424768ce91867f7328e3
Artem Vasilev <kern.usr@gmail.com>
2020-03-24 02:26:40 +0300

    Дописал оформление заказа и отправку на сервер

--- com_jshopping_addon_courierexe/sm_courierexe_form/sm_courierexe_form.php
+++ com_jshopping_addon_courierexe/sm_courierexe_form/sm_courierexe_form.php
@@ -23,7 +23,7 @@ class sm_courierexe_form extends ShippingFormRoot
 		$app      = Factory::getApplication();
 		$doc      = $app->getDocument();
 		$language = Factory::getLanguage();
-		$language->load('lib_measoft', JPATH_SITE, $language->getTag(), true);
+		$language->load('com_jshopping_addon_courierexe', JPATH_SITE, $language->getTag(), true);
 		$connection = MeasoftCourier::getInstance();
 
 		$shipping_params = unserialize($shippinginfo->params);
@@ -38,133 +38,131 @@ class sm_courierexe_form extends ShippingFormRoot
 			$user_info = &JSFactory::getUserShopGuest();
 		}
 
+		$cityto = '';
+
+		if (isset($user_info->d_city) && !empty($user_info->d_city))
+		{
+			$cityto = $user_info->d_city;
+		}
+		else
+		{
+			if (isset($user_info->city) && !empty($user_info->city))
+			{
+				$cityto = $user_info->city;
+			}
+		}
+
+		if (empty($cityto))
+		{
+			echo '<div class="alert"><h5>' . Text::_('COM_JSHOPPING_ADDON_COURIEREXE_ERROR_PRICE_CALC_TITLE') . '</h5>' . Text::_('COM_JSHOPPING_ADDON_COURIEREXE_ERROR_CITY_NOT_SPECIFIED_DESCRIPTION') . '</div>';
+
+			return;
+		}
+
+		if (!$shippinginfo->calculeprice)
+		{
+			echo '<div class="alert"><h5>' . Text::_('COM_JSHOPPING_ADDON_COURIEREXE_ERROR_PRICE_CALC_TITLE') . '</h5>' . Text::_('COM_JSHOPPING_ADDON_COURIEREXE_ERROR_PRICE_CALC_DESCRIPTION') . '</div>';
+		}
+
 		//var_dump($shipping_params);
 		if ($shipping_params['show_pvz'])
 		{
+			echo '<input type="hidden" name="params[' . $shipping_id . '][sm_courierexe]" value="true" />';
+
 			$pvzParams = array();
-
-			$cityto = '';
-
-			if (isset($user_info->d_city) && !empty($user_info->d_city))
-			{
-				$cityto = $user_info->d_city;
-			}
-			else
-			{
-				if (isset($user_info->city) && !empty($user_info->city))
-				{
-					$cityto = $user_info->city;
-				}
-			}
-
-			if (strlen($cityto) > 0)
-			{
-				$arrTown = $connection->citiesList(array('namecontains' => strtolower($cityto), 'country' => 1),
-					array('limitcount' => 1));
-				if (!empty($arrTown))
-				{
-					$townArr = array_shift($arrTown);
-					$cityto  = $townArr['name'];
-				}
-				else
-				{
-					$cityto = '';
-				}
-			}
 
 			$pvzParams['town'] = $cityto;
 
-			if (!empty($pvzParams['town']))
+			if (isset($shipping_params['acceptcash']))
 			{
-				if (isset($shipping_params['acceptcash']))
+				$pvzParams['acceptcash'] = $shipping_params['acceptcash'];
+			}
+
+			if (isset($shipping_params['acceptcard']))
+			{
+				$pvzParams['acceptcard'] = $shipping_params['acceptcard'];
+			}
+
+			if (isset($shipping_params['acceptfitting ']))
+			{
+				$pvzParams['acceptfitting '] = $shipping_params['acceptfitting '];
+			}
+
+			if (isset($shipping_params['acceptindividuals ']))
+			{
+				$pvzParams['acceptindividuals '] = $shipping_params['acceptindividuals '];
+			}
+
+			$doc->addScriptDeclaration('var pvzParams = ' . json_encode($pvzParams,
+					JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE) . ';');
+
+			if ($shipping_params['show_pvz_list'])
+			{
+				HTMLHelper::_('stylesheet', 'com_jshopping_addon_courierexe/select2.min.css',
+					array('relative' => true, 'version' => 'auto'));
+				HTMLHelper::_('script', 'com_jshopping_addon_courierexe/select2.min.js',
+					array('relative' => true, 'version' => 'auto'));
+
+				if (!$shipping_params['show_pvz_list_ajax'])
 				{
-					$pvzParams['acceptcash'] = $shipping_params['acceptcash'];
-				}
-
-				if (isset($shipping_params['acceptcard']))
-				{
-					$pvzParams['acceptcard'] = $shipping_params['acceptcard'];
-				}
-
-				if (isset($shipping_params['acceptfitting ']))
-				{
-					$pvzParams['acceptfitting '] = $shipping_params['acceptfitting '];
-				}
-
-				if (isset($shipping_params['acceptindividuals ']))
-				{
-					$pvzParams['acceptindividuals '] = $shipping_params['acceptindividuals '];
-				}
-
-				$doc->addScriptDeclaration('var pvzParams = ' . json_encode($pvzParams,
-						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE) . ';');
-
-				if ($shipping_params['show_pvz_list'])
-				{
-					HTMLHelper::_('stylesheet', 'com_jshopping_addon_courierexe/select2.min.css',
-						array('relative' => true, 'version' => 'auto'));
-					HTMLHelper::_('script', 'com_jshopping_addon_courierexe/select2.min.js',
-						array('relative' => true, 'version' => 'auto'));
-
-					if (!$shipping_params['show_pvz_list_ajax'])
+					try
 					{
-						try
-						{
-							$pvzList = $connection->servicesPvzlist($pvzParams);
-							$pvz     = [];
+						$pvzList = $connection->servicesPvzlist($pvzParams);
+						$pvz     = [];
 
-							if ($pvzList)
+						if ($pvzList)
+						{
+							foreach ($pvzList as $item)
 							{
-								foreach ($pvzList as $item)
+								if (!empty($item->code) && !empty($item->address))
 								{
-									if (!empty($item->code) && !empty($item->address))
+									if (!empty($item->parentname))
 									{
-										if (!empty($item->parentname))
-										{
-											$pvz[] = [
-												'id'   => '[' . $item->code . '](' . $item->parentname . ') ' . $item->address,
-												'text' => '(' . $item->parentname . ') ' . $item->address
-											];
-										}
-										else
-										{
-											$pvz[] = [
-												'id'   => '[' . $item->code . '] ' . $item->address,
-												'text' => $item->address->__toString()
-											];
-										}
+										$pvz[] = [
+											'id'   => $item->code->__toString(),
+											'text' => '(' . $item->parentname->__toString() . ') ' . $item->address->__toString()
+										];
+									}
+									else
+									{
+										$pvz[] = [
+											'id'   => $item->code->__toString(),
+											'text' => $item->address->__toString()
+										];
 									}
 								}
-								$doc->addScriptDeclaration('var pvzData = ' . json_encode($pvz,
-										JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE) . ';');
-								$doc->addScriptDeclaration('
+							}
+							$doc->addScriptDeclaration('var pvzData = ' . json_encode($pvz,
+									JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE) . ';');
+							$doc->addScriptDeclaration('
 								jQuery(document).ready(function() {
-									jQuery(\'[name="params[' . $shipping_id . '][sm_courierexe_pickup]"]\').
+									jQuery(\'[name="params[' . $shipping_id . '][sm_courierexe_pvz_id]"]\').
 										select2({
 											data: pvzData,
 										});
 								});
 								');
-							}
-						}
-						catch (Exception $e)
-						{
-							echo '<div class="alert">Не удалось загрузить список ПВЗ</div>';
-							saveToLog("courierexe.log", $e->getCode() . ' - ' . $e->getMessage());
 						}
 					}
-					echo HTMLHelper::_('select.genericlist', [], 'params[' . $shipping_id . '][sm_courierexe_pickup]');
+					catch (Exception $e)
+					{
+						echo '<div class="alert alert-error">' . Text::_('COM_JSHOPPING_ADDON_COURIEREXE_ERROR_PVZ_LIST_NOT_LOADED_DESCRIPTION') . '</div>';
+						saveToLog("courierexe.log", $e->getCode() . ' - ' . $e->getMessage());
+
+						return;
+					}
 				}
-			}
-			else
-			{
-				echo '<div class="alert">Не удалось загрузить список ПВЗ</div>';
+				echo HTMLHelper::_('select.genericlist', [], 'params[' . $shipping_id . '][sm_courierexe_pvz_id]');
 			}
 		}
 	}
 
 	function getDisplayNameParams()
 	{
-		return array('sm_courierexe_form' => 'ID пункта выдачи');
+		return ['sm_courierexe_pvz_id' => 'Код ПВЗ в системе:',
+			'sm_courierexe_pvz_name' => 'Наименование ПВЗ:',
+			'sm_courierexe_pvz_parentname' => 'Наименование родительского элемента:',
+			'sm_courierexe_pvz_address' => 'Адрес ПВЗ:',
+			];
 	}
 }
--- plg_jshoppingcheckout_courierexe/courierexe.php
+++ plg_jshoppingcheckout_courierexe/courierexe.php
@@ -25,7 +25,97 @@ class plgJshoppingcheckoutCourierexe extends CMSPlugin
 		parent::__construct($subject, $config);
 	}
 
-	public function onAjaxCourierexe()
+	public function onBeforeCreateOrder(&$order, &$cart, &$checkout)
+	{
+		$shipping_params_data = $order->getShippingParamsData();
+
+		if (!$shipping_params_data['sm_courierexe'])
+		{
+			return;
+		}
+
+		$shipping_method        = $checkout->getShippingMethod();
+		$shipping_method_params = unserialize($checkout->getShippingMethodPrice()->params);
+
+		$city = array_shift($this->getCitiesList(!empty($order->d_city) ? $order->d_city : $order->city, 1));
+
+		if (!empty($shipping_params_data['sm_courierexe_pvz_id']))
+		{
+			$shippingForm = $shipping_method->getShippingForm();
+
+			$filter = ['town' => $city['name'], 'code' => $shipping_params_data['sm_courierexe_pvz_id']];
+			$pvz    = array_shift($this->getPvzList($filter));
+
+			$shipping_params_data['sm_courierexe_pvz_name']       = $pvz->name->__toString();
+			$shipping_params_data['sm_courierexe_pvz_parentname'] = $pvz->parentname->__toString();
+			$shipping_params_data['sm_courierexe_pvz_address']    = $pvz->address->__toString();
+
+			if ($shippingForm)
+			{
+				$shippingForm->setParams($shipping_params_data);
+				$shipping_params_names  = $shippingForm->getDisplayNameParams();
+				$order->shipping_params = getTextNameArrayValue($shipping_params_names, $shipping_params_data);
+			}
+
+			$order->setShippingParamsData($shipping_params_data);
+		}
+
+		$newOrder = [
+			'orderno'         => $order->order_number,
+			'person'          => implode(' ', [$order->d_f_name . ' ' . $order->d_l_name . ' ' . $order->d_m_name]),
+			'phone'           => implode(', ', [$order->d_email, $order->d_phone, $order->d_mobil_phone]),
+			'town'            => $city['fiascode'],
+			'address'         => implode(' ',
+				[$order->d_street, $order->d_street_nr, $order->d_home, $order->d_apartment]),
+			'weight'          => saveAsPrice($cart->getWeightProducts()),
+			'service'         => $shipping_method_params['shipping_service'],
+			'price'           => $order->order_total,
+			//Настроить связь типов оплаты и методов оплаты
+			'paytype'         => 'CASH',
+			'deliveryprice'   => $order->jshop_price_shipping,
+			'acceptpartially' => 'NO'
+		];
+
+		$products = [];
+		foreach ($cart->products as $product)
+		{
+			$products[] = [
+				'item'     => $product['product_name'],
+				'quantity' => $product['quantity'],
+				'mass'     => $product['weight'],
+				'retprice' => $product['price'],
+				'type'     => 1,
+				'extcode'  => $product['ean']
+			];
+		}
+
+		if(!$this->setOrder($newOrder, $products)){
+			saveToLog('courierexe.log', 'Не удалось оформить заказ');
+		}
+	}
+
+	protected function getCitiesList($str, $limit = 10)
+	{
+		if (strlen($str) > 0)
+		{
+			$result = $this->connection->citiesList(array('namecontains' => strtolower($str), 'country' => 1),
+				array('limitcount' => $limit));
+		}
+
+		return !empty($result) ? $result : [];
+	}
+
+	protected function getPvzList($filter)
+	{
+		return $this->connection->servicesPvzlist($filter);
+	}
+
+	protected function setOrder($data, $items)
+	{
+		return $this->connection->orderCreate($data, $items);
+	}
+
+	protected function onAjaxCourierexe()
 	{
 		$input = Factory::getApplication()->input;
 		$get   = $input->get->getArray();
@@ -45,13 +135,5 @@ class plgJshoppingcheckoutCourierexe extends CMSPlugin
 		return true;
 	}
 
-	public function getCitiesList($str, $limit = 10)
-	{
-		if (strlen($str) > 0)
-		{
-			$result = $this->connection->citiesList(array('namecontains' => strtolower($str), 'country' => 1),
-				array('limitcount' => $limit));
-		}
-		return !empty($result) ? $result : [];
-	}
+
 }
