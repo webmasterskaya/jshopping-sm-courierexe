57f006535291d3e6ab699c6fa9a0bdb2305343b7
Artem Vasilev <kern.usr@gmail.com>
2020-04-16 21:57:57 +0300

    1.0.0-rc2

--- script.php
+++ script.php
@@ -4,7 +4,7 @@ use Joomla\CMS\Factory;
 
 /**
  * @package    JShopping - Courierexe shipping
- * @version    1.0.0-rc
+ * @version    1.0.0-rc2
  * @author     Artem Vasilev - webmasterskaya.xyz
  * @copyright  Copyright (c) 2020 Webmasterskaya. All rights reserved.
  * @license    GNU General Public License version 3 or later; see LICENSE.txt
