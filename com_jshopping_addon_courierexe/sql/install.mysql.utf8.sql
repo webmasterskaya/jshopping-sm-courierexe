57cb78566ec6cae2237fe355d98f716e7a0193b1
Artem Vasilev <kern.usr@gmail.com>
2020-06-11 14:22:52 +0300

    install sql

---
+++ com_jshopping_addon_courierexe/sql/install.mysql.utf8.sql
@@ -0,0 +1,14 @@
+/**
+ * @package    JShopping - Courierexe shipping
+ * @version    1.0.0-rc2
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE.txt
+ * @link       https://webmasterskaya.xyz/
+ * @since      1.0.0
+ */
+
+insert into `#__jshopping_shipping_ext_calc` (`name`, `alias`, `description`, `params`, `shipping_method`,
+                                              `published`, `ordering`)
+values ('\"Курьерская служба 2008\"', 'sm_courierexe',
+        'Подключение к программе доставки \"Курьерская служба 2008\"', '', '', '1', '1');
---
+++ com_jshopping_addon_courierexe/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,11 @@
+/**
+ * @package    JShopping - Courierexe shipping
+ * @version    1.0.0-rc2
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU General Public License version 3 or later; see LICENSE.txt
+ * @link       https://webmasterskaya.xyz/
+ * @since      1.0.0
+ */
+
+ delete from `#__jshopping_shipping_ext_calc` where `alias` = 'sm_courierexe';
